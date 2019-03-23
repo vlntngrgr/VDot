e33e9b1f7d8c5d0072e53c24ae6a0578bb90e0e0
Valentin Gregoire <valentin.gregoire91@gmail.com>
2019-03-23 18:38:42 +0000

    Workspace update

---
+++ scripts/workspace.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+mkdir -p ~/Works/01-BeCode/00-Hors_routes 
+mkdir -p ~/Works/01-BeCode/01-La_prairie 
+mkdir -p ~/Works/01-BeCode/02-La_colline/01-PHP 
+mkdir -p ~/Works/01-BeCode/02-La_colline/02-NodeJS 
+mkdir -p ~/Works/99-Projet_Perso 
+
+git clone https://github.com/vlntngrgr/Learning-Environment.git ~/Works/01-BeCode/01-La_prairie/01-Learning_Environment 
+git clone https://github.com/vlntngrgr/error404.git ~/Works/01-BeCode/01-La_prairie/02-Error404 
+git clone https://github.com/vlntngrgr/main-eventer.git ~/Works/01-BeCode/01-La_prairie/03-Main_Eventer 
+git clone https://github.com/vlntngrgr/Mon-CV.git ~/Works/01-BeCode/01-La_prairie/04-Mon_CV 
+git clone https://github.com/vlntngrgr/VCard.git ~/Works/01-BeCode/01-La_prairie/05-VCard 
+git clone https://github.com/vlntngrgr/Star-Wars-Crawl.git ~/Works/01-BeCode/01-La_prairie/06-Star_Wars_Crawl 
+git clone https://github.com/vlntngrgr/starting-web-developpment.git ~/Works/01-BeCode/01-La_prairie/00-Starting_Web_Dev 
+git clone https://github.com/vlntngrgr/javascript.git ~/Works/01-BeCode/01-La_prairie/07-Javascript 
+
+git clone https://github.com/vlntngrgr/trombecode.git ~/Works/01-BeCode/00-Hors_routes/01-Trombecode 
+git clone https://github.com/vlntngrgr/express-lessons.git ~/Works/01-BeCode/00-Hors_routes/02-Express_Lessons 
+git clone https://github.com/AnarionBe/BashIt.git ~/Works/01-BeCode/00-Hors_routes/03-BashIt
+
+git clone https://github.com/vlntngrgr/php-messenger.git ~/Works/01-BeCode/02-La_colline/01-PHP/01-Messenger 
+git clone https://github.com/vlntngrgr/theTheatre.git ~/Works/01-BeCode/02-La_colline/01-PHP/02-theTheatre 
+
+git clone https://github.com/vlntngrgr/pwned-cli.git ~/Works/01-BeCode/02-La_colline/02-NodeJS/01-Pwned 
+git clone https://github.com/vlntngrgr/pomodoro.git ~/Works/01-BeCode/02-La_colline/02-NodeJS/02-Pomodoro 
+git clone https://github.com/Stev07/Bookshelf.git ~/Works/01-BeCode/02-La_colline/02-NodeJS/03-Bookshelf 
+git clone https://github.com/AnarionBe/met.git ~/Works/01-BeCode/02-La_colline/02-NodeJS/04-met 
+
+git clone https://github.com/vlntngrgr/atss.git ~/Works/01-BeCode99-Projet_Perso/01-ATSS 
+git clone https://github.com/vlntngrgr/LIE-Hamilton-1.7.git ~/Works/01-BeCode/99-Hamilton 
+cd ~/Works/01-BeCode/99-Hamilton 
+git remote add upstream https://github.com/becodeorg/LIE-Hamilton-1.7.git 
+cd ~/ 
+git clone https://github.com/vlntngrgr/webpath.git ./99-Projet_Perso/99-WebDev 
