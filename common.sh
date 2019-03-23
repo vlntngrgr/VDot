2a26cdb52b99db55aca409af63213e91715ef5be
Valentin Gregoire <valentin.gregoire91@gmail.com>
2019-03-23 18:22:15 +0000

    Update folders and path

--- common.sh
+++ common.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
- 
+export DOTFILES_DIR="~/.config/dotfiles"
+
 echo ""
 echo "---------- VDot --------"
 echo ""
@@ -9,9 +10,13 @@ case $response in
     [yY]) echo "";;
     *) exit 1;;
 esac
- 
-mkdir -p ~/.config
+
+## Creating files and moving file where they should be
 mkdir -p ~/.modules
+cp -r ./dotfiles ~/.config
+
+sudo rm /etc/pacman.d/mirrorlist
+ln -svf "$DOTFILES/mirrorlist" /etc/pacman.d/mirrorlist 
 
 sudo pacman -Suy
 
@@ -30,5 +35,23 @@ fi
 if ! type "zsh" > /dev/null; then
 	"y" | sudo pacman -Su zsh
 fi
+
+echo ""
+echo "------ link dotfiles ------"
+
+ln -sfv "$DOTFILES_DIR/babelrc.json" ~/.babelrc
+ln -sfv "$DOTFILES_DIR/curlrc" ~/.curlrc
+ln -sfv "$DOTFILES_DIR/editorconfig" ~/.editorconfig
+ln -sfv "$DOTFILES_DIR/eslintrc.js" ~/.eslintrc.js
+ln -sfv "$DOTFILES_DIR/gitconfig" ~/.gitconfig
+ln -sfv "$DOTFILES_DIR/gitignore_global" ~/.gitignore_global
+ln -sfv "$DOTFILES_DIR/prettierrc" ~/.prettierrc
+ln -sfv "$DOTFILES_DIR/wgetrc" ~/.wgetrc
+
+echo ""
+echo "----- link terminfo files -----"
+
+tic -x "$DOTFILES_DIR/terminfo/xterm-256color-italic.terminfo"
+tic -x "$DOTFILES_DIR/terminfo/tmux-256color.terminfo"
 
 sh ./scripts/01-omz.sh
